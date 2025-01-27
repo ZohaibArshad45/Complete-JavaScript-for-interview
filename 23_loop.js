2a63773eada8fb4e67119098df3fe2b7685ebe9d
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-27 21:08:02 +0500

    Loops

---
+++ 23_loop.js
@@ -0,0 +1,25 @@
+// // for loop
+// for(let i= 0; i<10 ; i++){
+//         console.log(i)
+// }
+
+
+// // while loop 
+// let i = 0
+// while (i<10) {
+//     console.log(i)
+//     i++
+    
+// }
+
+
+// for of loop
+const arr = [1,2,3,4,5]
+for (const j of arr) {
+    console.log(j)
+}
+
+const stringw = 'Zohaib'
+for (const j of stringw) {
+    console.log(j)
+}
