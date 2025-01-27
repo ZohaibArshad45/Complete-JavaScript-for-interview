4786770596aac01219c1b09ded5c7c934577179e
Zohaib Arshad <zohaibfta@gmail.com>
2025-01-27 19:42:41 +0500

    truthy and falsy

---
+++ 21_Execution.js
@@ -0,0 +1,16 @@
+// JavaScript Excution Context
+// - Global execution context
+// - Function execution context
+// - Block execution context
+
+// memory stack
+// local scope
+// global scope
+
+// creation phase
+// execution phase
+
+// memory stack
+// memory heap
+
+
